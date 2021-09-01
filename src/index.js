2a05432bdb35682e2053d82501f280fc567ff8cc
irynakarmazinova <i.karmazinova1992@gmail.com>
2021-09-01 23:53:57 +0300

    added pnotify

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import fetchMovie from './js/fetchApi';
+
+// pnotify
+import { defaultModules, error } from '@pnotify/core';
+import '@pnotify/core/dist/PNotify.css';
+import * as PNotifyMobile from '@pnotify/mobile';
+import '@pnotify/mobile/dist/PNotifyMobile.css';
+import '@pnotify/core/dist/BrightTheme.css'; //color theme for error
+defaultModules.set(PNotifyMobile, {});
