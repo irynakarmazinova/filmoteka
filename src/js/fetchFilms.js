acb95159c1551845c481ba7a4b92f0de4b09f724
Serhii-Zvieriev <zacjok@gmail.com>
2021-09-04 14:11:25 +0300

    fix btn-load-more

--- src/js/fetchFilms.js
+++ src/js/fetchFilms.js
@@ -1,10 +1,8 @@
 import API from './fetchApi';
 import movieTmpl from '../templates/movie-card.hbs';
-import { /*searchForm,*/ gallery /*, loadBtn, container*/ } from './refs';
+import { gallery } from './refs';
 
 const api = new API();
-
-// loadBtn.addEventListener('click', onClick);
 
 api
   .fetchMovie()
@@ -14,23 +12,3 @@ api
 export default function renderMovieCard(movie) {
   gallery.insertAdjacentHTML('beforeend', movieTmpl(movie));
 }
-
-// function onClick(e) {
-//   e.preventDefault();
-//   api
-//     .fetchMovie()
-//     .then(data => {
-//       renderMovieCard(data);
-//     //   scrollEnd();
-//     })
-//     .catch(error => console.log(error));
-// }
-
-// function scrollEnd() {
-//   setTimeout(() => {
-//     container.scrollIntoView({
-//       behavior: 'smooth',
-//       block: 'end',
-//     });
-//   }, 800);
-// }
