cd1e8805727cdc12473cc96ddbba28ab93dffd9b
Vadym Polishchuk <77321951+Vadym-P@users.noreply.github.com>
2021-09-05 16:04:26 +0300

    updated classe on the btns

--- src/js/markup-home.js
+++
@@ -1,8 +0,0 @@
-export default function markupHomePage() {
-  return `<form class="search-form">
-    <label for="searchQuery" class="sr-only"></label>
-    <input class="search" type="text" name="query"
-    id="searchQuery" placeholder="Movie search" autocomplete="off"/>
-    <button class="button-search" type="submit"></button>
-  </form>`
-}
