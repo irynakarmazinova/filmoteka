b6ca886923b24feaa7bb86740f2775bc35528a82
Dimon-Stasyuk <78039077+Dimon-Stasyuk@users.noreply.github.com>
2021-09-05 16:21:11 +0300

    made a command modal window

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,8 +1,13 @@
-const refs= {
-    searchForm: document.querySelector('.search-form'),
-    gallery: document.querySelector('.gallery'),
-    loadBtn: document.querySelector('.btn'),
-    container: document.getElementById('js-container'),
+const refs = {
+  searchForm: document.querySelector('.search-form'),
+  gallery: document.querySelector('.gallery'),
+  loadBtn: document.querySelector('.btn'),
+  container: document.getElementById('js-container'),
+  backdrop: document.querySelector('.backdrop'),
+  modalCloseBtn: document.querySelector('.team__modal-close-btn'),
+  modal: document.querySelector('.modal'),
+  footerLink: document.getElementById('footer__link'),
 };
-  
-export const { searchForm, gallery, loadBtn, container } = refs;
+
+export const { searchForm, gallery, loadBtn, container, backdrop, modalCloseBtn, footerLink } =
+  refs;
