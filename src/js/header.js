3f788b62496433beb5a0f17dd48b61b5c64978dd
Vadym Polishchuk <77321951+Vadym-P@users.noreply.github.com>
2021-09-05 03:42:46 +0300

    added murkup for my library

--- src/js/header.js
+++ src/js/header.js
@@ -1,33 +1,88 @@
 import API from './fetchApi';
 import movieTmpl from '../templates/movie-card.hbs';
-import { searchForm, gallery, loadBtn, container} from './refs';
+import {
+  searchForm,
+  gallery,
+  loadBtn,
+  container,
+  logo,
+  homeBtn,
+  myLibraryBtn,
+  header,
+  btnWatched,
+  btnQueue,
+  btnContainerLibrary,
+} from './refs';
 
 const api = new API();
 
-searchForm.addEventListener('submit', onSearch)
+homeBtn.addEventListener('click', onHomeBtnClick);
+myLibraryBtn.addEventListener('click', onLibraryBtnClick);
+logo.addEventListener('click', onLogoClick);
+searchForm.addEventListener('submit', onSearch);
+btnWatched.addEventListener('submit', onSubmitWatched);
+btnWatched.addEventListener('click', onSubmitWatched);
+btnQueue.addEventListener('submit', onSubmitQueue);
+btnQueue.addEventListener('click', onSubmitQueue);
 
+function onHomeBtnClick(e) {
+  e.preventDefault();
+  btnContainerLibrary.classList.add('visually-hidden');
+  searchForm.classList.remove('visually-hidden');
+  header.classList.remove('header-library');
 
-
-function renderMovieCard(movie) {
-   gallery.insertAdjacentHTML('beforeend', movieTmpl(movie))
+  homeBtn.classList.add('current');
+  myLibraryBtn.classList.remove('current');
 }
 
+function onLibraryBtnClick(e) {
+  e.preventDefault();
+  searchForm.classList.add('visually-hidden');
+  btnContainerLibrary.classList.remove('visually-hidden');
+  header.classList.add('header-library');
 
+  homeBtn.classList.remove('current');
+  myLibraryBtn.classList.add('current');
+}
+
+function onLogoClick(e) {
+  e.preventDefault();
+  btnContainerLibrary.classList.add('visually-hidden');
+  searchForm.classList.remove('visually-hidden');
+  header.classList.remove('header-library');
+
+  homeBtn.classList.add('current');
+  myLibraryBtn.classList.remove('current');
+}
+
+function renderMovieCard(movie) {
+  gallery.insertAdjacentHTML('beforeend', movieTmpl(movie));
+}
 
 function onSearch(e) {
   e.preventDefault();
- clearMovieCard();
+  clearMovieCard();
   api.query = e.currentTarget.elements.query.value;
   api.resetPage();
-   api.fetchSearch()
-        .then(renderMovieCard)
-        .catch(error => console.log(error))
+  api
+    .fetchSearch()
+    .then(renderMovieCard)
+    .catch(error => console.log(error));
   e.currentTarget.elements.query.value = '';
 }
 
-
-
 function clearMovieCard() {
-    gallery.innerHTML = '';
+  gallery.innerHTML = '';
+}
 
+function onSubmitWatched(e) {
+  e.preventDefault();
+  btnWatched.classList.add('accent-color');
+  btnQueue.classList.remove('accent-color');
+}
+
+function onSubmitQueue(e) {
+  e.preventDefault();
+  btnQueue.classList.add('accent-color');
+  btnWatched.classList.remove('accent-color');
 }
--- src/js/refs.js
+++ src/js/refs.js
@@ -1,8 +1,16 @@
-const refs= {
-    searchForm: document.querySelector('.search-form'),
-    gallery: document.querySelector('.gallery'),
-    loadBtn: document.querySelector('.btn'),
-    container: document.getElementById('js-container'),
+const refs = {
+  searchForm: document.querySelector('.search-form'),
+  gallery: document.querySelector('.gallery'),
+  loadBtn: document.querySelector('.btn'),
+  container: document.getElementById('js-container'),
+  logo: document.getElementById('logo'),
+  homeBtn: document.querySelector('.menu__link'),
+  myLibraryBtn: document.getElementById('my-library'),
+  header: document.querySelector('.header'),
+  btnWatched: document.getElementById('btn-watched'),
+  btnQueue: document.getElementById('btn-queue'),
+  btnContainerLibrary: document.querySelector('.btn-container__library'),
+
 };
-  
-export const { searchForm, gallery, loadBtn, container } = refs;
+
+export const { searchForm, gallery, loadBtn, container, logo, homeBtn, myLibraryBtn, header, btnWatched, btnQueue, btnContainerLibrary, btnItem } = refs;
